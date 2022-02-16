6c5f839269026102ccf93a1bf41763fcf36dd4f8
kimminkyu <kmk50009@naver.com>
2022-02-16 21:46:29 +0900

    Add Generics

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,100 @@
-// Code goes here!
+// const names: Array<string> = []; //string[]
+// // names[0].split(' ');
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   // data.split(' ');
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "elliot" }, { age: 30 });
+console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+
+function extractAndConvert<T extends Object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "elliot" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("elliot");
+textStorage.addItem("elliotelon");
+textStorage.removeItem("elliot");
+console.log(textStorage.getItems());
+
+const numberStoarage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const elliotObj = { name: "elliot" };
+// objStorage.addItem(elliotObj);
+// objStorage.addItem({ name: "elon" });
+
+// objStorage.removeItem(elliotObj);
+// console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['elliot', 'elon']
+// names.push('telon')
